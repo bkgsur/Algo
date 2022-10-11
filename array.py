c5c42dbaebfe3527be3e046367cc0be4688b34a0
Suresh Gopalakrishnan <suresh.gopalakrishnan@imanage.com>
2022-10-11 13:26:02 -0700

    dp1

---
+++ array.py
@@ -0,0 +1,61 @@
+import itertools
+# fixed window
+def maxsubarray(A,k):
+    maxsum = 0
+    if len(A)<k or k<=0:
+        return 0
+    for i in range(len(A)):
+        if(i<k):
+            maxsum+=A[i]
+        else:
+            maxsum= max(maxsum , maxsum+A[i] - A[i-k])
+    return maxsum
+
+#print(maxsubarray([4,2,1,7,8,1,2,8,1,0],3))
+
+# sliding window
+def smallestsubarraySize(A, k):
+    i =0
+    j=0
+    minlength = 0
+    currentsum=0
+    while  j<len(A) and i<=j:
+        currentsum = sum(A[i:j+1])
+        if currentsum>=k :
+            if j-i+1 == 1:
+                return 1
+            else:
+                minlength = min(minlength, j-i+1)
+                i+=1
+        else:
+            j+=1
+    return  minlength
+
+#print(smallestsubarraySize([4,2,2,7,8,1,2,8,10],8))
+
+#sliding window - extra storage
+def longestuniquechars(S,k):
+    d ={}
+    maxlength=0
+    U=[]
+    left=0
+    for i in range(len(S)):
+        c = S[i]
+        if c in d.keys():
+            d[c]+=1
+        else:
+            d[c]=1
+            U.append(c)
+        if len(d)<=k:
+            maxlength = max(maxlength, i - left + 1)
+        else:
+            d.pop(U[left])
+            left+=1
+
+    return maxlength
+
+print(longestuniquechars(['A','A','A','H','H','I','B','C'],2))
+
+
+
+
--- honors.py
+++ honors.py
@@ -1,3 +1,11 @@
+def pm(a):
+    s = [[str(e) for e in row] for row in a]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
+    print('=============================')
+
 def gcd(x, y):
     # make sure y >x
     if x > y:
@@ -25,4 +33,31 @@ def missingpositive(A):
     return next((i + 1 for i, a in enumerate(A) if a != i + 1), len(A) + 1)
 
 
-print(missingpositive([3, 5, 4, -1, 5, 1, -1]))
+#print(missingpositive([3, 5, 4, -1, 5, 1, -1]))
+
+#https://www.youtube.com/watch?v=Pw6lrYANjz4
+def buysellk(prices,k):
+    print(prices)
+    #can hold only on stock at a time
+    profits = [ [0 for _ in range(len(prices))]for _ in range(k+1)]
+    for attempt in range(len(profits)): # length = k+1
+        profit_not_selling_today =0
+        profit_selling_today =0
+        for day in range(len(profits[attempt])):
+            # no profits can be made with attempt=0  and  day = 0 (you cannot make profit buying and selling on day 1)
+            if( attempt >0 and day>0):
+                # Maximum of scenarios 1 and 2
+                #1. Profit made by not selling today - so profit from yesterday till current attempt
+                #2. profit is max of profit made selling today from current attempt(todays price)
+                        # + maximum profit made from previous attempt on any previous day- cost of buying on that previous day's price.
+                profit_not_selling_today  = profits[attempt][day-1]
+                #previous days
+                max_previous_profit = float('-inf')
+                for previous_day in range(0,day):
+                    max_previous_profit = max(max_previous_profit, profits[attempt-1][previous_day] - prices[previous_day])
+                profit_selling_today = prices[day] + max_previous_profit
+                profits[attempt][day]= max(profit_not_selling_today, profit_selling_today)
+    pm(profits)
+    return profits[-1][-1]
+
+print(buysellk([5,11,3,50,60,90],2))
