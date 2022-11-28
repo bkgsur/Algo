64149379f1845fe7f72c8f0cd2344eb003464d3f
bkgsur <bkgsur@yahoo.com>
2022-11-28 14:54:53 -0800

    sort

--- final/arrayproblems.py
+++ final/arrayproblems.py
@@ -20,7 +20,7 @@ def watercontainer(H=[]) -> (int, (int, int)):
 
 # print(watercontainer([1, 8, 6, 2, 5, 4, 8, 3, 7]))
 
-
+# 283. Move Zeros
 def movezerotofront(A: []) -> None:
     zeros = []
     l = -1
@@ -35,21 +35,76 @@ def movezerotofront(A: []) -> None:
             l += 1
     if l == -1:
         l = 0
-    #print(zeros, l)
+    # print(zeros, l)
 
     for i in zeros:
         if i - l > 0:
             A[i], A[l] = A[l], A[i]
-            k =l+1
+            k = l + 1
 
-            while i-k>0:
+            while i - k > 0:
                 A[i], A[k] = A[k], A[i]
-                k+=1
-        l+=1
+                k += 1
+        l += 1
 
 
-A = [0,1, 3, 0, 12, 17, 0, 6, 0]
+# A = [0,1, 3, 0, 12, 17, 0, 6, 0]
+#
+# print(A)
+# movezerotofront(A)
+# print(A)
 
-print(A)
-movezerotofront(A)
-print(A)
+# 26. Remove Duplicates from Sorted Array
+# Given a sorted arraynums, remove the duplicates in-place such that each element appear only _once _and return the new length.
+#
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+def removeDuplicates(nums):
+    if len(nums) == 0:
+        return 0
+    fast = 0
+    slow = 0
+    length = 1
+    while fast < len(nums):
+        if nums[fast] == nums[slow]:
+            fast += 1
+        else:
+            slow += 1
+            print(slow, fast)
+            nums[slow] = nums[fast]
+            fast += 1
+            length += 1
+    print(nums)
+    return length
+
+
+# print(removeDuplicates([1, 2, 2]))
+# print(removeDuplicates([1, 1, 2]))
+
+
+# print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+
+def merge(nums1, m, nums2, n):
+    i = m-1
+    j = n-1
+    total = m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[total] = nums1[i]
+            i -= 1
+        else:
+            nums1[total] = nums2[j]
+            j -= 1
+        total -= 1
+
+    if i < 0:
+        while j >= 0:
+            nums1[total] = nums2[j]
+            j -= 1
+    if j < 0:
+        while i >= 0:
+            nums1[total] = nums1[i]
+            i -= 1
+    return nums1
+
+print(merge([1,2,3,0,0,0],3,[2,5,6],3))
